7d79af02757275c8e37762d92c4674c5b50f3220
miguelsdcardoso <miguel.cardoso@datatalks.se>
2022-10-31 16:36:32 +0000

    v1

--- src/schema_compare/main.py
+++ src/schema_compare/main.py
@@ -1,17 +1,14 @@
-import tomli
 import sys
-from drivers.postgresql.connection import Connection
+import tomli
+from schema_compare.drivers.postgresql.connection import Connection
 
-
-
-if __name__ == "__main__":
-
+def main():
     if len(sys.argv) < 2:
         raise Exception("Please provide the file path of your configuration.toml file")
 
-    file_path = sys.argv[1]
+    FILE_PATH = sys.argv[1]
 
-    with open(file_path, mode="rb") as fp:
+    with open(FILE_PATH, mode="rb") as fp:
         config = tomli.load(fp)
 
     source = config["source"]
@@ -30,14 +27,25 @@ if __name__ == "__main__":
     print(f'Target schema has {len(source)} (table_name,column_name)')
 
     result = source[source.apply(not_exists, axis = 1)]
-
+    print("The following are not present in target schema:")
+    print(result)
     print(f"Target schema has less {len(result)} (table_name,column_name) than source schema")
     result = result.to_html()
-    
-    f = open("report.html","r")
+
+    import os
+    absolute_path = os.path.dirname(__file__)
+    relative_path = "report.html"
+    full_path = os.path.join(absolute_path, relative_path)
+
+    f = open(full_path,"r")
+
     content = f.read()
 
-    output = open("output.html","w")
+    output = open("/output.html","w")
     output.write(content.replace("{{body}}",result))
+    output.close()
+    f.close()
 
-
+if __name__ == "__main__":
+    main()
+   
