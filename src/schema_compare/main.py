d76f4f4813b3a61a04f2b0f7b1cf516f0a814dd2
miguelsdcardoso <miguel.cardoso@datatalks.se>
2022-12-08 12:20:21 +0000

    bugfix: not_exists method

--- src/schema_compare/main.py
+++ src/schema_compare/main.py
@@ -22,7 +22,8 @@ def main():
     def not_exists(row):
         table_name = row["table_name"]
         column_name = row["column_name"]
-        return ((table_name  == target["table_name"]).sum() + (column_name  == target["column_name"]).sum()) == 0
+
+        return ((table_name  == target["table_name"]).sum() * (column_name  == target["column_name"]).sum()) == 0
 
     print(f'Source schema has {len(source)} (table_name,column_name)')
     print(f'Target schema has {len(target)} (table_name,column_name)')
